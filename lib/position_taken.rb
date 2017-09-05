c5ba7161a90b121856706fb2010129068601bf6e
Matt Hough <matt@hough.tv>
2017-09-05 09:31:51 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,5 +1,7 @@
 def position_taken?(index, board)
-  if board[index] == " " || board[index] == ""
+  if board[index] == " " || board[index] == "" || board[index] == nil
     return false
+  else
+    return true
   end
 end
