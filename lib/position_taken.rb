ab2f691f8e7eabd43e362f2beb99eeb8cf670933
Matt Hough <matt@hough.tv>
2017-09-05 08:57:38 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,14 +1,3 @@
-# code your #position_taken? method here!
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-index = 1
-
-def position_taken?(index, board)
-  if board[index] == " " || ""
-    board[index] = "X"
-  else
-    puts "That space is already full"
-  end
+def position_taken?
+  
 end
-
-position_taken?(index, board)
-puts board
