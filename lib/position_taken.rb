26795e9356a0fc64ace2045feebc5fe661d65370
Matt Hough <matt@hough.tv>
2017-09-05 09:13:50 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,9 +1,3 @@
 def position_taken?(index, board)
-  if board[index] == !(" ") || !("")
-    print "Failed"
-  else
-    print "I think you've done it!"
-  end
-end
 
-position_taken?(1, [" ", " ", " ", " ", " ", " ", " ", " ", " "])
+end
